7dbb026e1055f32f750be58878ad2adbecf5a048
HashedDan <georgedanielmangum@gmail.com>
2018-10-18 18:04:47 -0500

    changed port cause heroku is mad at me

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const Tabletop = require("tabletop")
 
 var app = express()
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send("SEND ME YOUR CODES")
