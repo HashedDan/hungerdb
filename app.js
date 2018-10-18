7d5562f5d42d184c72de4d7e3ad6470b4f402bc3
HashedDan <georgedanielmangum@gmail.com>
2018-10-18 17:56:02 -0500

    well that was easy

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const Tabletop = require("tabletop")
+
+var app = express()
+var port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send("SEND ME YOUR CODES")
+})
+
+app.get('/:code', (req, res) => {
+    Tabletop.init( { key: 'https://docs.google.com/spreadsheets/d/1r0SBy6aZZ-Q3mPqCfgTuJ9WAHhVUa3xECY3_qcmEoRM/edit?usp=sharing',
+                   callback: function(data, tabletop) {
+                       var element = data.find((assassin) => {
+                            return assassin.code === req.params.code
+                       })
+                       res.send(JSON.stringify(element))
+                   },
+                   simpleSheet: true } )
+})
+
+app.listen(port, () => {
+    console.log("app running on port: ", port);
+})
